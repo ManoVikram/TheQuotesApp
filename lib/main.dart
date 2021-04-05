09486a082f0d56f9b65566e45848eb34d92b759d
Mano Vikram <manovik18@gmail.com>
2021-04-05 16:06:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Quotes App",
+      debugShowCheckedModeBanner: false,
+      home: QuotesApp(),
+    );
+  }
+}
+
+class QuotesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
